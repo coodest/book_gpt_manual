e29eb9dc0685a7aa17e6066b299c09292eabdf02
ZiduYin <zidu.yin@outlook.com>
2025-02-10 13:57:47 +0000

    250210 initial submit

--- run.sh
+++ run.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-case "$OSTYPE" in
-    linux*) 
-    echo "linux platform"
-    pdflatex="pdflatex"
-    bibtex="bibtex";;
-esac
+
+# latex="pdflatex"
+latex="xelatex"
+bibtex="bibtex"
 main="main"
 target=$main
-parg="-file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=./ ${target}.tex"
+# parg="-file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=./ ${target}.tex"
+parg="-file-line-error -interaction=nonstopmode -synctex=1 -output-directory=./ ${target}.tex"
 
 start=$SECONDS
 rm -f *.log
-$pdflatex ${parg} 2>&1 > /dev/null
+$latex ${parg} 2>&1 > /dev/null
 if [ "$target" == "$main" ]; 
 then
 $bibtex ${target} 2>&1 > /dev/null
 fi
-$pdflatex ${parg} 2>&1 > /dev/null
-$pdflatex ${parg} 2>&1 > /dev/null
+$latex ${parg} 2>&1 > /dev/null
+$latex ${parg} 2>&1 > /dev/null
 rm -f *.aux
 rm -f *.bbl
 rm -f *.blg
